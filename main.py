5bf06c453a1f1d7d0193ae45713daf2feac05014
mutelnwza <mutemallow@gmail.com>
2025-02-17 16:19:34 +0700

    add comments

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSend
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
-load_dotenv()
 
-# Initialize FastAPI app
+load_dotenv()
 app = FastAPI()
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
@@ -21,15 +19,12 @@ handler = WebhookHandler(CHANNEL_SECRET)
 # Webhook endpoint
 @app.post("/callback")
 async def callback(request: Request):
-    # Get signature
     signature = request.headers.get("X-Line-Signature", "")
     
-    # Get request body
     body = await request.body()
     body_text = body.decode("utf-8")
 
     try:
-        # Handle webhook
         handler.handle(body_text, signature)
     except InvalidSignatureError:
         raise HTTPException(status_code=400, detail="Invalid signature")
@@ -41,11 +36,13 @@ async def callback(request: Request):
 def handle_message(event):
     user_message = event.message.text.lower()
 
+    # send image to user
     if user_message == "send img":
         img_url = 'https://i.ibb.co/xcKgTxY/cheems.png'
         line_bot_api.reply_message(
             event.reply_token, ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
         )
+    # text
     else:
         reply = f"You said: {user_message}"
         line_bot_api.reply_message(
