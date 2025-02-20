ae7e223e70c61a4d04ddda30e6f4833865e9554f
mutelnwza <mutemallow@gmail.com>
2025-02-20 14:17:50 +0700

    db

---
+++ db.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname = "linebot_database_4mwg",
+    user= 'linebot_database_4mwg_user',
+    password = 'uSVf4L5FZOA3SEkRj69csPuyDUhULMnS',
+    host='dpg-curc23rqf0us73fhs56g-a.singapore-postgres.render.com',
+    port='5432'
+    )
+
+cursor = conn.cursor()
+
+def store_message(sender, user_id, user_name, message)
+{
+    query = "INSERT INTO {sender} (user_id, name, time, text) VALUES ({id}, {name}, NOW();, {text})".format(sender = sender, id = user_id, name = user_name, text = message)
+    cursor.execute(query)
+    conn.commit()
+}
--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 
 import os
+import db
+
 from dotenv import load_dotenv
 
 
 load_dotenv()
 app = FastAPI()
+
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
@@ -21,6 +24,8 @@ if not ACCESS_TOKEN or not CHANNEL_SECRET:
 
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
+
+
 
 # Webhook endpoint
 @app.post("/callback")
@@ -40,10 +45,13 @@ async def callback(request: Request):
 # Handle text messages
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    user_id = event.source.user_id # get user id
+    name = line_bot_api.get_profile(user_id).display_name
     user_message = event.message.text.lower()
 
     # send image to user
     if user_message == "send img":
+        db.store_message("user_db", user_id, name, user_message)
         img_url = 'https://i.ibb.co/xcKgTxY/cheems.png'
         line_bot_api.reply_message(
             event.reply_token, ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
